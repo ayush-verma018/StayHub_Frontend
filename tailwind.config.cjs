228e5c2d4a79dbfc824dcff3d561625a8d29a281
ayush.verma3 <ayush.verma@infomatics.in>
2024-02-23 15:52:28 +0530

    frontend completed

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0077b6",
+      },
+    },
+  },
+  plugins: [],
+};
